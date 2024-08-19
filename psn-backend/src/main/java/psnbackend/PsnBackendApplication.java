14e39fde243d1d790075e5a9fdb1ad24d87d90aa
USER <thakursusmi94@gmail.com>
2024-08-19 10:09:45 +0545

    psn backend and frontend

---
+++ psn-backend/src/main/java/psnbackend/PsnBackendApplication.java
@@ -0,0 +1,13 @@
+package psnbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsnBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsnBackendApplication.class, args);
+	}
+
+}
